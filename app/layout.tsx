59ea6482b5671a00cef591f29788dbc29bf124d3
redwan9011 <redwan390309@gmail.com>
2024-03-15 23:06:59 +0600

    add babelsrc

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Nunito_Sans } from "next/font/google";
+// import { Nunito_Sans } from "next/font/google";
 import "./globals.css";
 
-const inter = Nunito_Sans({ subsets: ["cyrillic-ext"] });
+// const inter = Nunito_Sans({ subsets: ["cyrillic-ext"] });
 
 export const metadata: Metadata = {
   title: "Framer-Wave",
@@ -18,7 +18,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <link rel="icon" href="/Logo.png" sizes="any" />
-      <body className={inter.className}>{children}</body>
+      <body >{children}</body>
     </html>
   );
 }
+
+
+
+
+
+
+
